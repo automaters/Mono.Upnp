83e4ce91e4a89325dcc2e354f3ddee2151f83fed
amn3sia <amn3sia@gmail.com>
2011-02-13 14:32:34 -0800

    Fixed the last remaining unit test and made a minor correction to my previous fix because we really shouldn't care about attribute order in XML. So in both cases I allow the test to pass either way.

--- tests/Mono.Upnp.Tests/XmlSerializationTests.cs
+++ tests/Mono.Upnp.Tests/XmlSerializationTests.cs
@@ -689,9 +689,10 @@ namespace Mono.Upnp.Tests
         [Test]
         public void OverrideOmitTest ()
         {
-            Assert.AreEqual (
-                @"<?xml version=""1.0""?><OverrideOmitTestSubClass />",
-                serializer.GetString (new OverrideOmitTestSubClass { Foo = "bar" })
+            Assert.That (
+                serializer.GetString (new OverrideOmitTestSubClass { Foo = "bar" }),
+                Is.EqualTo(@"<?xml version=""1.0""?><OverrideOmitTestSubClass />").Or.EqualTo(
+                    @"<?xml version=""1.0""?><OverrideOmitTestSubClass></OverrideOmitTestSubClass>")
             );
             
             Assert.AreEqual (
@@ -795,9 +796,11 @@ namespace Mono.Upnp.Tests
         [Test]
         public void NamespaceTest ()
         {
-            Assert.AreEqual (
-                @"<?xml version=""1.0""?><NamespaceTestClass xmlns:b=""udn:mono-upnp:bar"" xmlns:f=""udn:mono-upnp:foo""><f:Foo>foo</f:Foo><b:Bar>bar</b:Bar></NamespaceTestClass>",
-                serializer.GetString (new NamespaceTestClass { Foo = "foo", Bar = "bar" }));
+            Assert.That (
+                serializer.GetString (new NamespaceTestClass { Foo = "foo", Bar = "bar" }),
+                Is.EqualTo(@"<?xml version=""1.0""?><NamespaceTestClass xmlns:b=""udn:mono-upnp:bar"" xmlns:f=""udn:mono-upnp:foo""><f:Foo>foo</f:Foo><b:Bar>bar</b:Bar></NamespaceTestClass>").
+                Or.EqualTo(@"<?xml version=""1.0""?><NamespaceTestClass xmlns:f=""udn:mono-upnp:foo"" xmlns:b=""udn:mono-upnp:bar""><f:Foo>foo</f:Foo><b:Bar>bar</b:Bar></NamespaceTestClass>")
+            );
         }
         
         class ValueTestClass
